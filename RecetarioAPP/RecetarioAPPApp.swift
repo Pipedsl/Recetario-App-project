7bf78ca777e1e4755ef93a6e48fe6d99d84df0b7
Pipedsl <h.navarrete@duocuc.cl>
2025-09-25 13:35:47 -0300

    Initial Commit

---
+++ RecetarioAPP/RecetarioAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecetarioAPPApp.swift
+//  RecetarioAPP
+//
+//  Created by Felipe Navarrete Navarrete on 25-09-25.
+//
+
+import SwiftUI
+
+@main
+struct RecetarioAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecetarioAPPTests/RecetarioAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecetarioAPPTests.swift
+//  RecetarioAPPTests
+//
+//  Created by Felipe Navarrete Navarrete on 25-09-25.
+//
+
+import Testing
+@testable import RecetarioAPP
+
+struct RecetarioAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
